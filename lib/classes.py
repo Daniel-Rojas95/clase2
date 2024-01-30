109993aa6db2492a2bcb226c2be50c7faeb4e4d0
torres9560@gmail.com <MontseG700@gmail.com>
2024-01-30 08:31:34 -0600

    precio classes

---
+++ lib/classes.py
@@ -0,0 +1,24 @@
+class articulo():
+    def __init__(self, idArt, marca, nombre, precio=None, peso=None, dcto=None, inv=None):
+        self.idArt = idArt
+        self.marca=marca
+        self.nombre=nombre
+        self.precio=precio
+        self.peso=peso
+        self.dcto=dcto
+        self.inv=inv
+        pass
+    
+    def __str__(self):
+        return f"idArt: {self.idArt} - marca: {self.marca} - nombre:{self-nombre} - precio:{self.precio} - peso:{self.peso} - dcto: {self.dcto} - inv:{self.inv}"
+    
+    def setPrecio(self, price):
+        self.precio=price
+        pass 
+
+    
+
+    pass
+
+class carritou():
+    pass
